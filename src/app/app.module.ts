7f4b40572cdd69a9dd4fd757521a2bccc4ced0b1
fawzy20 <fawzy-elsam@outlook.fr>
2023-09-19 22:09:35 +0200

    ajout des function d'inscription et de ces contrainte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,37 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { HomePageComponent } from './models/home-page/home-page.component';
+import { ConnexionComponent } from './models/connexion/connexion.component';
+import { NouveauCompteComponent } from './models/nouveau-compte/nouveau-compte.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 const appRoutes: Routes = [
-  { path: '', component: HomePageComponent },
-  // Ajoutez d'autres routes ici si nécessaire
+  { path: '', component: HomePageComponent, pathMatch: 'full'},
+  { path: 'home', component: HomePageComponent },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'connexion/nouveauCompte', component: NouveauCompteComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomePageComponent
+    HomePageComponent,
+    NouveauCompteComponent,
+    ConnexionComponent
   ],
   imports: [
+    FormsModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/service/user-service.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../dataModels/user';
+import { Observable } from 'rxjs';
+
+
+@Injectable({providedIn: 'root'})
+export class UserService {
+
+  private usersUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.usersUrl = 'http://localhost:8081';
+  }
+
+  public findUserByMail(mail: String): Observable<User> {
+    return this.http.get<User>(`${this.usersUrl}/usersByMail?mail=${mail}`);
+  }
+
+  public addUser(user: User) {
+    return this.http.post<User>(this.usersUrl + '/nouveauCompte', user);
+  }
+
+}
