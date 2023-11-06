5d02cc1fd2caced8da252f90e6b4c0175b437509
fawzy20 <fawzy-elsam@outlook.fr>
2023-11-06 20:29:33 +0100

    test

--- src/app/models/connexion/connexion.component.ts
+++ src/app/models/connexion/connexion.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/service/user-service.service';
+
+import { User } from 'src/app/dataModels/user';
 
 @Component({
   selector: 'app-connexion',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./connexion.component.css']
 })
 export class ConnexionComponent {
+  errorConnexion: boolean = false;
+  user: User;
+
+  constructor(
+
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+
+  ) { this.user = new User();}
+
+            
+  gotoAdminHome() {
+    this.router.navigate(['/home']);
+  }
+  
+  onSubmit(){
+   this.userService.login(this.user).subscribe(result => {
+    if(result != null){
+      this.errorConnexion = false;
+      this.gotoAdminHome(); 
+    }else{
+      localStorage.clear();
+      this.errorConnexion = true;
+      console.log("les identifiant sont incorect " + this.errorConnexion);
+      
+    }
+   })
+  }
 
 }
